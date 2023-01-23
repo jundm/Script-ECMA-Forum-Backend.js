5b84ea8d77e389f3b0671d81a769c8949ebc1bcc
Logan <80582578+jundm@users.noreply.github.com>
2023-01-23 21:50:34 +0900

    Feature/1.docker setting (#2)
    * add backend directory;
    
    * add docker.dev, docker-compose.dev;
    
    * add nginx;

--- backend/src/data-source.ts
+++ backend/src/data-source.ts
@@ -3,15 +3,12 @@ import { DataSource } from "typeorm"
 
 process.env.NODE_ENV = ( process.env.NODE_ENV && ( process.env.NODE_ENV ).trim().toLowerCase() == 'production' ) ? 'production' : 'development';
 require('dotenv').config({path: `.env.${process.env.NODE_ENV}`});
-console.log(process.env.NODE_ENV,'ho');
 export const AppDataSource = new DataSource({
     type: "postgres",
-    host: "localhost",
+    host: "postgres",
     port: 5432,
     username: process.env.POSTGRES_USER,
     password: process.env.POSTGRES_PASSWORD,
-    // username: 'jun',
-    // password: 'tjdwns11',
     database: "postgres",
     synchronize: true, //dev only
     logging: false,
