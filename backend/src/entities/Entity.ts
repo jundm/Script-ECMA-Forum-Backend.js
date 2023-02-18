aafdace0c8702250553c92e0e06dfa918ad8df77
devjun <devjun@devjunui-MacBook-Pro.local>
2023-02-19 00:47:04 +0900

    add baseenttiy;

---
+++ backend/src/entities/Entity.ts
@@ -0,0 +1,17 @@
+import {BaseEntity, CreateDateColumn, PrimaryGeneratedColumn, UpdateDateColumn} from "typeorm";
+import {instanceToPlain} from "class-transformer";
+
+export default abstract class Entity extends BaseEntity {
+    @PrimaryGeneratedColumn()
+    id: number
+
+    @CreateDateColumn()
+    createdAt: Date;
+
+    @UpdateDateColumn()
+    updatedAt: Date;
+
+    toJSON() {
+        return instanceToPlain(this);
+    }
+}
