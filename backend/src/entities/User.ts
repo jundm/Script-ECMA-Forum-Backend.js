1eb56fa414e71a7614134c11fcf8d622f8a37882
devjun <devjun@devjunui-MacBook-Pro.local>
2023-02-19 17:38:32 +0900

    add user entity;

--- backend/src/entities/User.ts
+++ backend/src/entities/User.ts
@@ -1,18 +1,22 @@
-// import { Entity, PrimaryGeneratedColumn, Column } from "typeorm"
-//
-// @Entity()
-// export class User {
-//
-//     @PrimaryGeneratedColumn()
-//     id: number
-//
-//     @Column()
-//     firstName: string
-//
-//     @Column()
-//     lastName: string
-//
-//     @Column()
-//     age: number
-//
-// }
+import {Entity, Column, Index, OneToMany, BeforeInsert} from "typeorm";
+import {IsEmail, Length} from "class-validator";
+import bcrypt from "bcryptjs";
+import BaseEntity from '@entities/Entity';
+
+@Entity('users')
+export class User extends BaseEntity {
+    @Index()
+    @IsEmail()
+    @Length(1, 255, {message: "이메일 주소가 잘못되었습니다."})
+    @Column({unique: true})
+    email: string;
+
+    @Index()
+    @Length(3, 32, {message: "사용자 이름은 3자 이상이여야 합니다."})
+    @Column({unique: true})
+    username: string;
+
+    @Column()
+    @Length(6, 255, {message: "비밀번호는 ㅣ자리 이사이영야 합니다."})
+    password: string;
+}
